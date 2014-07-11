c8c7a0bfc704cd2e3b9d1b09bb7415afcac2a359
Victor Purcallas <vpurcallas@gmail.com>
2014-07-11 19:30:07 +0200

    Improving main class

--- src/Model/Main.java
+++ src/Model/Main.java
@@ -2,14 +2,19 @@ package Model;
 
 
 
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Iterator;
+
+import javax.jws.Oneway;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 public class Main {
+	
+	public static String analyzedUrl = "http://www.elmundo.es/ciencia/2014/07/10/53beb7d3ca4741f8298b4586.html";
 
 	/**
 	 * @param args
@@ -20,28 +25,58 @@ public class Main {
 	}
 	
 	public static void run(){
+		moreComments();
 	
-	try{
-		URL url = new URL("http://www.elmundo.es/ciencia/2014/07/10/53beb7d3ca4741f8298b4586.html"); 
-		Document doc = Jsoup.parse(url,15000);
-		System.out.println("load html");
-		
-		
-		System.out.println("Comments");
-		System.out.println();
-		
-		Iterator<Element> listaTitle = doc.select("article").iterator();
-		
-		while (listaTitle.hasNext()){
+//	try{
+//		URL url = new URL(analyzedUrl); 
+//		Document doc = Jsoup.parse(url,15000);
+//		System.out.println("load html");
+//		
+//		
+//		System.out.println("Comments");
+//		System.out.println();
+//		
+//		Iterator<Element> listaTitle = doc.select("article").iterator();
+//		
+//		while (listaTitle.hasNext()){
+//			
+//			Element e = listaTitle.next();		
+//			String comment = e.select("div[class^=texto-comentario]").select("p").text();		
+//			System.out.println(comment);
+//		}
+//		
+//	}catch(Exception e){
+//		e.printStackTrace();
+//	}
+}
+
+	private static void moreComments() {
+		try{
+			URL url = new URL(analyzedUrl); 
+			Document doc = Jsoup.parse(url,15000);
+			System.out.println("load html");
 			
-			Element e = listaTitle.next();		
-			String comment = e.select("div[class^=texto-comentario]").select("p").text();		
-			System.out.println(comment);
+			
+			System.out.println("Comments");
+			System.out.println();
+			
+			Iterator<Element> listaTitle = doc.select("ul[id^=subNavComentarios]>li>a[id^=botonMas]").iterator();
+			
+			while (listaTitle.hasNext()){
+				
+				Element e = listaTitle.next();
+				String onClick = e.attr("onclick").toString();
+				System.out.println(onClick);
+//				Jsoup.connect(analyzedUrl).data(onClick);
+//				Jsoup.connect(analyzedUrl).data()
+				String more = e.toString();
+				System.out.println(more);
+			}
+			
+		}catch(Exception e){
+			e.printStackTrace();
 		}
 		
-	}catch(Exception e){
-		e.printStackTrace();
 	}
-}
 
 }
