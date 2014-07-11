3640bbf75e219ffd571a509ebc2711353b7119e0
Victor Purcallas <vpurcallas@gmail.com>
2014-07-11 20:32:53 +0200

    Improve the comments parse using selenium

--- src/Model/Main.java
+++ src/Model/Main.java
@@ -1,19 +1,23 @@
 package Model;
 
-
-
-import java.net.MalformedURLException;
-import java.net.URL;
+//import java.net.URL;
 import java.util.Iterator;
 
-import javax.jws.Oneway;
+//import org.jsoup.Jsoup;
+//import org.jsoup.nodes.Document;
+//import org.jsoup.nodes.Element;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
+import org.openqa.selenium.*;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.support.ui.ExpectedCondition;
+//import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Main {
-	
+
 	public static String analyzedUrl = "http://www.elmundo.es/ciencia/2014/07/10/53beb7d3ca4741f8298b4586.html";
 
 	/**
@@ -23,60 +27,71 @@ public class Main {
 		run();
 
 	}
-	
-	public static void run(){
+
+	public static void run() {
 		moreComments();
-	
-//	try{
-//		URL url = new URL(analyzedUrl); 
-//		Document doc = Jsoup.parse(url,15000);
-//		System.out.println("load html");
-//		
-//		
-//		System.out.println("Comments");
-//		System.out.println();
-//		
-//		Iterator<Element> listaTitle = doc.select("article").iterator();
-//		
-//		while (listaTitle.hasNext()){
-//			
-//			Element e = listaTitle.next();		
-//			String comment = e.select("div[class^=texto-comentario]").select("p").text();		
-//			System.out.println(comment);
-//		}
-//		
-//	}catch(Exception e){
-//		e.printStackTrace();
-//	}
-}
+
+		// try{
+		// URL url = new URL(analyzedUrl);
+		// Document doc = Jsoup.parse(url,15000);
+		// System.out.println("load html");
+		//
+		//
+		// System.out.println("Comments");
+		// System.out.println();
+		//
+		// Iterator<Element> listaTitle = doc.select("article").iterator();
+		//
+		// while (listaTitle.hasNext()){
+		//
+		// Element e = listaTitle.next();
+		// String comment =
+		// e.select("div[class^=texto-comentario]").select("p").text();
+		// System.out.println(comment);
+		// }
+		//
+		// }catch(Exception e){
+		// e.printStackTrace();
+		// }
+	}
 
 	private static void moreComments() {
-		try{
-			URL url = new URL(analyzedUrl); 
-			Document doc = Jsoup.parse(url,15000);
-			System.out.println("load html");
-			
-			
-			System.out.println("Comments");
-			System.out.println();
-			
-			Iterator<Element> listaTitle = doc.select("ul[id^=subNavComentarios]>li>a[id^=botonMas]").iterator();
-			
-			while (listaTitle.hasNext()){
-				
-				Element e = listaTitle.next();
-				String onClick = e.attr("onclick").toString();
-				System.out.println(onClick);
-//				Jsoup.connect(analyzedUrl).data(onClick);
-//				Jsoup.connect(analyzedUrl).data()
-				String more = e.toString();
-				System.out.println(more);
+		Boolean exit = false;
+		try {
+//			URL url = new URL(analyzedUrl);
+//			Document doc = Jsoup.parse(url, 15000);
+//			System.out.println("load html");
+//
+//			System.out.println("Comments");
+//			System.out.println();
+			WebDriver driver = new FirefoxDriver();
+			driver.get(analyzedUrl);
+
+			// Repeat the show more click until all the commentaries are showed
+			while (!exit) {
+				try {
+					WebElement element = driver
+							.findElement(By
+									.xpath("//ul[@id='subNavComentarios']//li//a[@id='botonMas']"));
+					element.click();
+				} catch (Exception e) {
+					exit = true;
+				}
 			}
-			
-		}catch(Exception e){
+
+			Iterator<WebElement> listaTitle = driver.findElements(By.xpath("//div[@class='texto-comentario']/p[text()]")).iterator();
+
+			while (listaTitle.hasNext()) {
+
+				WebElement e = listaTitle.next();
+				String comment = e.getText();
+				System.out.println(comment);
+			}
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
