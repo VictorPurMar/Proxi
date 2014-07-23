5acc21d083c57aa1c94991885fbe9faee303d3cc
Victor Purcallas <vpurcallas@gmail.com>
2014-07-23 10:54:20 +0200

    Adapting to W

--- src/Controller/ProxiCSVWriter.java
+++ src/Controller/ProxiCSVWriter.java
@@ -9,33 +9,34 @@ import Model.Article;
 public class ProxiCSVWriter {
 
 	public static void makeTheCSV(Article article) {
+		final String divider = ";";
 
 		String name = article.getDate().replaceAll("-", "")
 				+ "_"
-				+ article.getTitle().replaceAll("^[0-9,.;:-_'\\s]+$", "")
-						.substring(0, 20) + ".csv";
+				+ article.getTitle().replaceAll("^[0-9,.;:-_'\\s]+$", "").replaceAll("\\s+","")
+						.substring(0, 20) + ".xml";
 		try {
+//			PrintWriter writer = new PrintWriter(name, "ISO-8859-3");
 			PrintWriter writer = new PrintWriter(name, "UTF-8");
-			writer.println("ART√çCULO");
-			writer.println(article.getTitle());
-			writer.println(article.getSubtitle());
-			writer.println(article.getDate());
-			writer.println(article.getAuthor());
-			writer.println(article.getDiary());
-			writer.println(article.getUrl());
+			writer.println("ARTÕCULO");
+			writer.println();
+			writer.println("Titulo" + divider + article.getTitle());
+			writer.println("Subtitulo" + divider +article.getSubtitle());
+			writer.println("Fecha" + divider +article.getDate());
+			writer.println("Autor" + divider +article.getAuthor());
+			writer.println("Diario" + divider +article.getDiary());
+			writer.println("Enlace" + divider +article.getUrl());
+			writer.println();
+			writer.println();
 			writer.println("COMENTARIOS");
 
 			for (int i = 0; i < article.getCommentaries().size(); i++) {
 				writer.println(article.getCommentaries().get(i).getNumber()
-						+ "###" + article.getCommentaries().get(i).getDate()
-						+ "###"
+						+ divider + article.getCommentaries().get(i).getDate()
+						+ divider
 						+ article.getCommentaries().get(i).getNickName()
-						+ "###"
+						+ divider
 						+ article.getCommentaries().get(i).getCommentary());
-				// writer.println(commentaries.get(i).getNumber() + "###" +
-				// commentaries.get(i).getDate() + "###" +
-				// commentaries.get(i).getNickName() + "###" +
-				// commentaries.get(i).getCommentary());
 			}
 			writer.close();
 
